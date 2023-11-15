5fd0fdfc30848171f1e8a92ed193f0874d779fe0
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-11-15 23:26:43 +0000

    use pathlib to find templates

--- FilmFlix/__init__.py
+++ FilmFlix/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from .Models import db
 from .views import views
-import json
-
+from pathlib import Path
 
 def create_app():
-    app = Flask(__name__, template_folder='FilmFlix-Web\FilmFlix\templates')
+    app = Flask(__name__, template_folder=Path('templates').resolve(strict=False))
     app.register_blueprint(views, ulr_prefix="/")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cheleaux:5iTFvLPyo3373Orp3W1RhprbipLAacuZ@dpg-clad289m6hds73dhqpi0-a/flimflix'
     db.init_app(app)
