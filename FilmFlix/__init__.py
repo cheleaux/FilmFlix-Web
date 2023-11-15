aaab0ecefbb7c38821445a6be7a1c5277a8ca618
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-11-15 23:44:29 +0000

    changed template path

--- FilmFlix/__init__.py
+++ FilmFlix/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from .Models import db
 from .views import views
-from pathlib import Path
 
 def create_app():
-    app = Flask(__name__, template_folder=Path('templates').absolute())
+    app = Flask(__name__, template_folder='\FilmFlix Web\FilmFlix\templates')
     app.register_blueprint(views, ulr_prefix="/")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cheleaux:5iTFvLPyo3373Orp3W1RhprbipLAacuZ@dpg-clad289m6hds73dhqpi0-a/flimflix'
     db.init_app(app)
