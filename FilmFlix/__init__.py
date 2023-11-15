eaf8bc21b74527d56ce3bff1655d0f471461396d
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-11-15 23:33:13 +0000

    pathlib template path to absolute

--- FilmFlix/__init__.py
+++ FilmFlix/__init__.py
@@ -4,7 +4,7 @@ from .views import views
 from pathlib import Path
 
 def create_app():
-    app = Flask(__name__, template_folder=Path('templates').resolve())
+    app = Flask(__name__, template_folder=Path('templates').absolute())
     app.register_blueprint(views, ulr_prefix="/")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cheleaux:5iTFvLPyo3373Orp3W1RhprbipLAacuZ@dpg-clad289m6hds73dhqpi0-a/flimflix'
     db.init_app(app)
