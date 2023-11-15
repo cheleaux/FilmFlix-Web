5248095851d3c12a5d896539b52df62377ee755b
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-11-15 22:23:33 +0000

    moved database uri

--- FilmFlix/__init__.py
+++ FilmFlix/__init__.py
@@ -7,8 +7,5 @@ import json
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(views, ulr_prefix="/")
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = "postgresql://cheleaux:5iTFvLPyo3373Orp3W1RhprbipLAacuZ@dpg-clad289m6hds73dhqpi0-a/flimflix"
     db.init_app(app)
     return app
